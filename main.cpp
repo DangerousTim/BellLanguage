7f8f8990131708c7c208e7910e30682e645d1507
Tim <DangerousTim@users.noreply.github.com>
2017-06-18 19:59:35 +0530

    Updated Lexer
    main.cpp is a test program

--- main.cpp
+++ main.cpp
@@ -23,18 +23,26 @@ extern Memory memory;
 = does solve lhs, store mem index, solve rhs, input at stored mem index
 this>>3>>3 should work but not this>3>>4 but yes this>>3>4
 */
-int main(void){
-	treeNode *root = NULL;
-	
+int main(void){	
 	char s[100];
 	cin.getline(s, 100);
 	
+	treeNode *root = NULL;
 	Lexeme *lexlist = createLexemeList(s);
+
 	Token *toklist = createTokenList(lexlist);
+	
+	for (int i = 0; toklist[i].name != tn_null; i++)
+		root = insertToken(root, toklist[i]);
+
+	cout<<"PostOrder: \n";	
+	postOrder(root);
+	
 	destroyLexemeList(lexlist);
 	destroyTokenList(toklist);
+	
+	cout<<"Answer: \n";
+	solve(root).print();
 
-
-	//postOrder(root);
 	deleteTree(root);
 }
