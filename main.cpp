cbd3766abd1476926d4e2bb6fcb4b763019c4401
SiddhantBarua <siddhant.barua13@gmail.com>
2017-07-16 19:41:12 +0530

    Readfile function
    Read messages in main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+#include <fstream>
 #include "lexer.h"
 #include "token.h"
 #include "parser.h"
 #include "interpreter.h"
 
-using std::cout;
-using std::cin;
+using namespace std;
+
+char buffer1[1023];
 
 extern Memory memory;
 extern short programExit;
 
-void executeLine(char s[]){	
+void executeLine(char s[]){
 
 	Lexeme *lexlist = createLexemeList(s);		//make lexeme list
 	Token *toklist = createTokenList(lexlist);	//make token list
-	
+
 	//unrecognized token present then toklist[0] has tokErr
 	if (toklist[0].name != tn_error){
 
@@ -28,7 +30,7 @@ void executeLine(char s[]){
 		cout<<"Post order:\n";
 		postOrder(root);
 		cout<<'\n';
-*/			
+*/
 		//check if there's a syntax error
 		//if there is, don't execute
 		if (syntax(root) != tn_error){
@@ -36,7 +38,7 @@ void executeLine(char s[]){
 			cout<<'\n';
 		}
 		else
-			cout<<"syntax error\n";	
+			cout<<"syntax error\n";
 
 		deleteTree(root);
 	}
@@ -46,9 +48,29 @@ void executeLine(char s[]){
 	destroyTokenList(toklist);
 }
 
-int main(void){
+int readfile(const char *filename){
+	ifstream file;
+	file.open(filename, ios::in);
 
-	while (!programExit){	
+    if (!file.is_open()) {
+        cout << "File " << filename << " does not exist.\n";
+     	return -1;
+    }
+
+    while(!file.eof()) {
+        file.getline(buffer1,1023);
+        buffer1[1023]='\0';
+        executeLine(buffer1);
+    }
+    return 0;
+}
+
+int main(void){
+    readfile("test.txt");   /*Only works if test.txt is in same directory as project
+                                Else, address of file has to be written
+                                    eg. C:\\Users\\Desktop\\Ron\\bell-master\\test.txt*/
+
+        while (!programExit){
 		char s[100];
 		cout<<"bell$ ";
 		cin.getline(s, 100);
